a948fb464e91ca12b461587657b799df1453703c
Zoe <zoejane207@gmail.com>
2026-04-15 14:59:15 +0100

    Added attributes test

--- tests/test_high_value.py
+++ tests/test_high_value.py
@@ -1,5 +1,11 @@
 from lib.high_value import *
 
+def test_attributes_are_correct_when_instantiated():
+    high_value = HighValue(3, 10)
+    result1 = high_value.value_first
+    result2 = high_value.value_second
+    assert result1 == 3 and result2 == 10
+
 def test_high_value_returns_first_value_is_higher():
     high_value = HighValue(10, 5)
     result = high_value.get_highest()
@@ -20,3 +26,8 @@ def test_high_value_returns_first_value_when_adding():
     high_value.add(2, "first")
     result = high_value.get_highest()
     assert result == "First value is higher"
+
+
+
+
+
